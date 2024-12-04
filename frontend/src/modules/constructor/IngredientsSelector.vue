5d63f9d44e07b930387f50160ada99ba393334b9
tyort <neotyo1986@gmail.com>
2024-12-04 23:32:07 +0300

    Стилизация

--- frontend/src/modules/constructor/IngredientsSelector.vue
+++ frontend/src/modules/constructor/IngredientsSelector.vue
@@ -49,7 +49,7 @@ import IngredientsSelectorButton from "./IngredientsSelectorButton.vue";
 import IngredientsSelectorCount from "./IngredientsSelectorCount.vue";
 import { getImage } from "../../common/helpers/normalize";
 
-const props = defineProps({
+defineProps({
   ingredients: {
     type: Array,
     default: () => [],
@@ -59,154 +59,31 @@ const props = defineProps({
 defineEmits(["changeIngredientAmount"]);
 </script>
 
-<!-- <style lang="scss" scoped>
-@import "@/assets/scss/ds-system/ds-colors.scss";
-@import "@/assets/scss/ds-system/ds-shadows.scss";
-@import "@/assets/scss/ds-system/ds-typography.scss";
-@import "@/assets/scss/mixins/m_center.scss";
+<style lang="scss" scoped>
+@import "@/assets/scss/ds-system/ds.scss";
+@import "@/assets/scss/mixins/mixins.scss";
 
-.counter {
+.ingredients__filling {
+  width: 100%;
+  p {
+    @include r-s16-h19;
+    margin-top: 0;
+    margin-bottom: 16px;
+  }
+}
+
+.ingredients__list {
+  @include clear-list;
   display: flex;
-
-  justify-content: space-between;
-  align-items: center;
+  align-items: flex-start;
+  flex-wrap: wrap;
 }
 
-.counter__button {
-  $el: &;
-  $size_icon: 50%;
-
-  position: relative;
-
-  display: block;
-
-  width: 16px;
-  height: 16px;
-  margin: 0;
-  padding: 0;
-
-  cursor: pointer;
-  transition: 0.3s;
-
-  border: none;
-  border-radius: 50%;
-  outline: none;
-
-  &--minus {
-    background-color: $purple-100;
-
-    &::before {
-      @include p_center-all;
-
-      width: $size_icon;
-      height: 2px;
-
-      content: "";
-
-      border-radius: 2px;
-      background-color: $black;
-    }
-
-    &:hover:not(:active):not(:disabled) {
-      background-color: $purple-200;
-    }
-
-    &:active:not(:disabled) {
-      background-color: $purple-300;
-    }
-
-    &:focus:not(:disabled) {
-      box-shadow: $shadow-regular;
-    }
-
-    &:disabled {
-      cursor: default;
-
-      &::before {
-        opacity: 0.1;
-      }
-    }
-  }
-
-  &--plus {
-    background-color: $green-500;
-
-    &::before {
-      @include p_center-all;
-
-      width: $size_icon;
-      height: 2px;
-
-      content: "";
-
-      border-radius: 2px;
-      background-color: $white;
-    }
-
-    &::after {
-      @include p_center-all;
-
-      width: $size_icon;
-      height: 2px;
-
-      content: "";
-      transform: translate(-50%, -50%) rotate(90deg);
-
-      border-radius: 2px;
-      background-color: $white;
-    }
-
-    &:hover:not(:active):not(:disabled) {
-      background-color: $green-400;
-    }
-
-    &:active:not(:disabled) {
-      background-color: $green-600;
-    }
-
-    &:focus:not(:disabled) {
-      box-shadow: $shadow-regular;
-    }
-
-    &:disabled {
-      cursor: default;
-
-      opacity: 0.3;
-    }
-  }
-
-  &--orange {
-    background-color: $orange-200;
-
-    &:hover:not(:active):not(:disabled) {
-      background-color: $orange-100;
-    }
-
-    &:active:not(:disabled) {
-      background-color: $orange-300;
-    }
-  }
-}
-
-.counter__input {
-  @include r-s14-h16;
-
-  box-sizing: border-box;
-  width: 22px;
-  margin: 0;
-  padding: 0 3px;
-
-  text-align: center;
-
-  color: $black;
-  border: none;
-  border-radius: 10px;
-  outline: none;
-  background-color: transparent;
-
-  &:focus {
-    box-shadow: inset $shadow-regular;
-  }
+.ingredients__item {
+  width: 100px;
+  min-height: 40px;
+  margin-right: 17px;
+  margin-bottom: 35px;
 }
 
 .ingredients__counter {
@@ -214,4 +91,27 @@ defineEmits(["changeIngredientAmount"]);
   margin-top: 10px;
   margin-left: 36px;
 }
-</style> -->
+
+.counter {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.filling {
+  @include r-s14-h16;
+  position: relative;
+  display: block;
+  padding-left: 36px;
+
+  img {
+    @include p_center-v;
+    display: block;
+    width: 32px;
+    height: 32px;
+    box-sizing: border-box;
+    padding: 4px;
+    border-radius: 50%;
+  }
+}
+</style>
--- frontend/src/modules/constructor/IngredientsSelectorCount.vue
+++ frontend/src/modules/constructor/IngredientsSelectorCount.vue
@@ -16,3 +16,26 @@ defineProps({
   },
 });
 </script>
+
+<style lang="scss" scoped>
+@import "@/assets/scss/ds-system/ds.scss";
+@import "@/assets/scss/mixins/mixins.scss";
+
+.counter__input {
+  @include r-s14-h16;
+  box-sizing: border-box;
+  width: 22px;
+  margin: 0;
+  padding: 0 3px;
+  text-align: center;
+  color: $black;
+  border: none;
+  border-radius: 10px;
+  outline: none;
+  background-color: transparent;
+
+  &:focus {
+    box-shadow: inset $shadow-regular;
+  }
+}
+</style>
