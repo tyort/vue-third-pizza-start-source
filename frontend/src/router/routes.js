99ed01d262ddbe6e8f086c5c4a363e21d18f4687
tyort <neotyo1986@gmail.com>
2024-12-13 13:51:45 +0300

    Создадим маршруты

--- frontend/src/router/routes.js
+++ frontend/src/router/routes.js
@@ -1,9 +1,34 @@
 export default [
   {
-    path: "/",
-    name: "HomeView",
-    component: () => import("../views/HomeView.vue"),
-    meta: { layout: "AppLayoutMain" },
-    children: [],
+    path: "",
+    name: "home",
+    component: () => import("@/views/HomeView.vue"),
+  },
+  {
+    path: "/login",
+    name: "login",
+    component: () => import("@/views/LoginView.vue"),
+  },
+  {
+    path: "/cart",
+    name: "cart",
+    component: () => import("@/views/CartView.vue"),
+  },
+  {
+    path: "/user",
+    name: "user",
+    component: () => import("@/views/UserView.vue"),
+    children: [
+      {
+        path: "orders",
+        name: "orders",
+        component: () => import("@/views/OrdersView.vue"),
+      },
+      {
+        path: "profile",
+        name: "profile",
+        component: () => import("@/views/ProfileView.vue"),
+      },
+    ],
   },
 ];
