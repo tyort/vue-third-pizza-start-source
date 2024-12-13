ca6f0d58068ce82266183d44daa20ed0d2b0e002
tyort <neotyo1986@gmail.com>
2024-12-13 13:57:39 +0300

    AppHeader to DefaultHeader

--- frontend/src/layouts/AppLayoutMain.vue
+++ frontend/src/layouts/AppLayoutMain.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
-    <app-header />
+    <default-header />
     <slot />
   </div>
 </template>
 
 <script setup>
-import AppHeader from "./AppHeader.vue";
+import DefaultHeader from "./DefaultHeader.vue";
 </script>
