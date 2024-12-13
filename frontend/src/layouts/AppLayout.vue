016fab858c5247fc091ad4d01a1352605956c9d2
tyort <neotyo1986@gmail.com>
2024-12-13 12:12:16 +0300

    3.14. Задание: маршрутизация и динамические лейауты в проекте Vue-Pizza

--- frontend/src/layouts/AppLayout.vue
+++ frontend/src/layouts/AppLayout.vue
@@ -1,10 +1,35 @@
 <template>
-  <div>
-    <app-header />
+  <component :is="layout">
     <slot />
-  </div>
+  </component>
 </template>
 
 <script setup>
-import AppHeader from "./AppHeader.vue";
+import { shallowRef, watch } from "vue";
+import { useRoute } from "vue-router";
+import AppLayoutDefault from "./AppLayoutDefault.vue";
+
+// для получения информации о текущем маршруте и его параметрах
+const route = useRoute();
+const layout = shallowRef(null);
+
+watch(
+  () => route.meta,
+  async (meta) => {
+    try {
+      if (meta.layout) {
+        const component = await import(`./${meta.layout}.vue`);
+        layout.value = component?.default || AppLayoutDefault;
+      } else {
+        layout.value = AppLayoutDefault;
+      }
+    } catch (err) {
+      console.error(
+        "Динамический шаблон не найден. Установлен шаблон по-умолчанию.",
+        err
+      );
+      layout.value = AppLayoutDefault;
+    }
+  }
+);
 </script>
