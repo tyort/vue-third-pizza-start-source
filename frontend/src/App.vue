9653443f05f5c821b331d2f6ded0e4521f67ce7d
tyort <neotyo1986@gmail.com>
2024-12-13 11:24:47 +0300

    Подключаем vue-router

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,10 +1,9 @@
 <template>
   <app-layout>
-    <home-view />
+    <router-view />
   </app-layout>
 </template>
 
 <script setup>
 import AppLayout from "@/layouts/AppLayout.vue";
-import HomeView from "@/views/HomeView.vue";
 </script>
