729edc2c678708a97c523be6479ff5480a2e332a
tyort <neotyo1986@gmail.com>
2025-01-04 23:51:17 +0300

    4.15. Задание: добавление геттеров хранилища в проект Vue-Pizza

--- frontend/src/stores/pizza.js
+++ frontend/src/stores/pizza.js
@@ -1,4 +1,5 @@
 import { defineStore } from "pinia";
+import { useDataStore } from "./data";
 
 export const usePizzaStore = defineStore("pizza", {
   state: () => ({
@@ -11,13 +12,46 @@ export const usePizzaStore = defineStore("pizza", {
     orderId: 0,
     ingredients: [
       {
-        id: 0,
-        pizzaId: 0,
         ingredientId: 0,
         quantity: 0,
       },
     ],
   }),
-  getters: {},
+  getters: {
+    extendedIngredsParams: (state) => {
+      const data = useDataStore();
+      return data.ingredientItems.map((ingred) => {
+        const currentIngredient = state.ingredients.find(
+          ({ ingredientId }) => ingredientId === ingred.id
+        );
+        return {
+          ...ingred,
+          quantity: currentIngredient ? currentIngredient.quantity : 0,
+        };
+      });
+    },
+    getFinalPizzaPrice: (state) => {
+      const data = useDataStore();
+
+      const sizeFactor = data.sizeItems.find(
+        (size) => size.id === state.sizeId
+      ).multiplier;
+
+      const doughPrice = data.doughItems.find(
+        (dough) => dough.id === state.doughId
+      ).price;
+
+      const saucePrice = data.sauceItems.find(
+        (sauce) => sauce.id === state.sauceId
+      ).price;
+
+      const ingredientsPrice = state.extendedIngredsParams
+        .filter(({ quantity }) => !!quantity && quantity > 0)
+        .map(({ price, quantity }) => price * quantity)
+        .reduce((finalPrice, price) => finalPrice + price, 0);
+
+      return sizeFactor * (doughPrice + saucePrice + ingredientsPrice);
+    },
+  },
   actions: {},
 });
