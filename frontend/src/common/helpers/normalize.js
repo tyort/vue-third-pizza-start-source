7ed76e2b86a94b800e790871ef6ff1316b7eff82
tyort <neotyo1986@gmail.com>
2025-01-12 21:21:36 +0300

    Рефакторинг кода

--- frontend/src/common/helpers/normalize.js
+++ frontend/src/common/helpers/normalize.js
@@ -3,35 +3,27 @@ import ingredients from "@/common/data/ingredients";
 import sauces from "@/common/data/sauces";
 import sizes from "@/common/data/sizes";
 
-export const normalizeDough = (dough) => {
-  return {
-    ...dough,
-    value: doughSizes[dough.id],
-  };
-};
+export const normalizeDough = (dough) => ({
+  ...dough,
+  value: doughSizes[dough.id],
+});
 
-export const normalizeSize = (size) => {
-  return {
-    ...size,
-    value: sizes[size.id],
-  };
-};
+export const normalizeSize = (size) => ({
+  ...size,
+  value: sizes[size.id],
+});
 
-export const normalizeIngredients = (ingredient) => {
-  return {
-    ...ingredient,
-    value: ingredients[ingredient.id],
-    quantity: 0,
-    ingredientId: ingredient.id,
-  };
-};
+export const normalizeIngredients = (ingredient) => ({
+  ...ingredient,
+  value: ingredients[ingredient.id],
+  quantity: 0,
+  ingredientId: ingredient.id,
+});
 
-export const normalizeSauces = (sauce) => {
-  return {
-    ...sauce,
-    value: sauces[sauce.id],
-  };
-};
+export const normalizeSauces = (sauce) => ({
+  ...sauce,
+  value: sauces[sauce.id],
+});
 
 export const getImage = (image) => {
   return new URL(`../../assets/img/${image}`, import.meta.url).href;
