9ed0211f581cc313fe540d947e998bb67af52324
tyort <neotyo1986@gmail.com>
2025-02-03 14:33:44 +0300

    Создание сервиса для работы с JWT-токеном

---
+++ frontend/src/services/jwt/jwt.service.js
@@ -0,0 +1,21 @@
+const ID_TOKEN_KEY = "token";
+
+class JwtService {
+  getToken() {
+    return window.localStorage.getItem(ID_TOKEN_KEY);
+  }
+
+  saveToken(token) {
+    window.localStorage.setItem(ID_TOKEN_KEY, token);
+  }
+
+  destroyToken() {
+    window.localStorage.removeItem(ID_TOKEN_KEY);
+  }
+}
+
+/*
+ * Сразу же экспортируем новый экземпляр класса,
+ * потому что сервис будет существовать в единственном экземпляре
+ */
+export default new JwtService();
